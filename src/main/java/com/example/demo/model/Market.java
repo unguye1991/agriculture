9edb6d33e22c4fe0d1a673431d045aac80adb793
unguye1991 <84440874+unguye1991@users.noreply.github.com>
2021-05-23 15:49:30 +0700

    Update Market.java

--- src/main/java/com/example/demo/model/Market.java
+++ src/main/java/com/example/demo/model/Market.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -18,7 +18,7 @@ public class Market {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name = "seller_id")
 	private Seller seller;
 	
