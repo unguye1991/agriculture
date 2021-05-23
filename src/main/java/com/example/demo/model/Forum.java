833208b4eec737b3ba7d2243ea38555ef7df1d25
unguye1991 <84440874+unguye1991@users.noreply.github.com>
2021-05-23 15:48:15 +0700

    Update Forum.java

--- src/main/java/com/example/demo/model/Forum.java
+++ src/main/java/com/example/demo/model/Forum.java
@@ -11,7 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-//import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -25,7 +25,7 @@ public class Forum {
   private int id;
  
  
-  @OneToOne
+  @ManyToOne
   @JoinColumn(name = "customer_nickname")
   private Customer customer;
  
