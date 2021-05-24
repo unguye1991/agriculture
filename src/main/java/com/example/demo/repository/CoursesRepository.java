894e0d9930509eb616aa15b757b704fb2c829903
unguye1991 <84440874+unguye1991@users.noreply.github.com>
2021-05-24 20:52:21 +0700

    Update CoursesRepository.java

--- src/main/java/com/example/demo/repository/CoursesRepository.java
+++ src/main/java/com/example/demo/repository/CoursesRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Courses;
-import com.example.demo.model.Customer;
+import com.example.demo.model.Seller;
 
 @Repository
 public interface CoursesRepository extends JpaRepository<Courses, Integer> {
-	public List<Courses> findByCustomer(Customer customer);
-	public List<Courses> findByCustomerId(int id);
+	public List<Courses> findBySeller(Seller seller);
+	public List<Courses> findBySellerId(int id);
 
 }
